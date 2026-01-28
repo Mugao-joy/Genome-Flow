17836484cdcb0d2e47f85cc364859c6d0a61aeb5
Mugao-joy <mugaojoy@gmail.com>
2026-01-28 18:09:28 +0300

    automatics QC trigger

--- backend/storage.py
+++ backend/storage.py
@@ -1,8 +1,7 @@
 import os
 import boto3
-from dotenv import load_dotenv
-
-load_dotenv()
+from botocore.client import Config
+from botocore.exceptions import ClientError
 
 S3_ENDPOINT = os.getenv("S3_ENDPOINT")
 S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY")
@@ -14,7 +13,25 @@ s3 = boto3.client(
     endpoint_url=S3_ENDPOINT,
     aws_access_key_id=S3_ACCESS_KEY,
     aws_secret_access_key=S3_SECRET_KEY,
+    config=Config(
+        s3={"addressing_style": "path"},
+        signature_version="s3v4",
+    ),
+    region_name="us-east-1",
 )
+
+def ensure_bucket_exists():
+    try:
+        s3.head_bucket(Bucket=BUCKET_NAME)
+    except ClientError as e:
+        error_code = int(e.response["Error"]["Code"])
+        if error_code == 404:
+            s3.create_bucket(Bucket=BUCKET_NAME)
+        else:
+            raise
+
+# 👇 run once on import
+ensure_bucket_exists()
 
 def upload_file(file_obj, object_name: str):
     s3.upload_fileobj(file_obj, BUCKET_NAME, object_name)
